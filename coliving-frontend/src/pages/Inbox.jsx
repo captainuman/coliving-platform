2b65cb940bc00bb970b710b9efea3298bf1ad59a
Numan <kingnuman2611@gmail.com>
2026-06-16 20:32:51 +0530

    sidebar

--- coliving-frontend/src/pages/Inbox.jsx
+++ coliving-frontend/src/pages/Inbox.jsx
@@ -12,7 +12,7 @@ export default function Inbox() {
     useState(location.state?.conversation || null);
 
   return (
-    <div className="min-h-screen bg-gray-400 overflow-hidden pb-10">
+    <div className="min-h-screen bg-gray-400 overflow-hidden pb-20">
       <Navbar />
 
       <div className="max-w-7xl mx-auto p-2 sm:p-4">
